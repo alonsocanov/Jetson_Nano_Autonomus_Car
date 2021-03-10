4f73d57eb09603b109b726201a722b15a57399dd
alonsocanov <alonsocanov@hotmail.com>
2021-03-09 20:57:32 -0600

    Added serial comunication

--- motor_control/motor_control.ino
+++ motor_control/motor_control.ino
@@ -17,10 +17,10 @@ class Motor
 private:
   int velocity(float value)
   {
-    // Map the potentiometer value from 0 to 255
-    int pwmOutput = map(value, 0, 1023, 0, 255);
-    return pwmOutput
-  }
+    // Map the potentiometer value from 0 to 255 from percentaje o to 100
+    int pwmOutput = map(value, 0, 100, 0, 255);
+    return pwmOutput;
+  };
 
 public:
   Motor(int enable, int input1, int input2)
@@ -39,74 +39,90 @@ public:
 
   void forward(float vel, float t)
   {
-    int pwm = velocity(vel)
-        analogWrite(en, pwm);
+    int pwm = velocity(vel);
+    analogWrite(en, pwm);
     digitalWrite(in1, LOW);
     digitalWrite(in2, HIGH);
-    delay(t);
   };
 
-  void backward(float vel, float t)
+  void backward(float vel)
   {
-    int pwm = velocity(vel)
-        analogWrite(en, pwm);
+    int pwm = velocity(vel);
+    analogWrite(en, pwm);
     digitalWrite(in1, HIGH);
     digitalWrite(in2, LOW);
-    delay(t);
   };
 
-  void stopMotor(float t)
+  void stopMotor()
   {
     analogWrite(en, LOW);
     digitalWrite(in1, LOW);
     digitalWrite(in2, LOW);
+  };
+
+  void time(float t)
+  {
     delay(t);
   };
 };
 
 void setup()
 {
+  //I2C COmunication
+  // initialize serial:
+  Serial.begin(9600);
+
   pinMode(enA, OUTPUT);
-  pinMode(in1, OUTPUT);
-  pinMode(in2, OUTPUT);
-  pinMode(button, INPUT);
+  pinMode(inA1, OUTPUT);
+  pinMode(inA2, OUTPUT);
+  // pinMode(button, INPUT);
   // Set initial rotation direction
-  digitalWrite(in1, LOW);
-  digitalWrite(in2, HIGH);
+  digitalWrite(inA1, LOW);
+  digitalWrite(inA2, LOW);
 }
 
 void loop()
 {
   // Read potentiometer value
-  int potValue = analogRead(A0);
+  // int potValue = analogRead(A0);
   // Map the potentiometer value from 0 to 255
-  int pwmOutput = map(potValue, 0, 1023, 0, 255);
+  // int pwmOutput = map(potValue, 0, 1023, 0, 255);
   // Send PWM signal to L298N Enable pin
-  analogWrite(enA, pwmOutput);
+  analogWrite(enA, 255);
 
   // Read button - Debounce
-  if (digitalRead(button) == true)
-  {
-    pressed = !pressed;
-  }
-  while (digitalRead(button) == true)
-    ;
-  delay(20);
+  // if (digitalRead(button) == true)
+  // {
+  //   pressed = !pressed;
+  // }
+  // while (digitalRead(button) == true)
+  //   ;
+  // delay(20);
 
   // If button is pressed - change rotation direction
-  if (pressed == true & rotDirection == 0)
-  {
-    digitalWrite(in1, HIGH);
-    digitalWrite(in2, LOW);
-    rotDirection = 1;
-    delay(20);
-  }
+  // if (pressed == true & rotDirection == 0)
+  // {
+  digitalWrite(inA1, HIGH);
+  digitalWrite(inA2, LOW);
+  delay(20);
+  // }
   // If button is pressed - change rotation direction
-  if (pressed == false & rotDirection == 1)
+  // if (pressed == false & rotDirection == 1)
+  // {
+  //   digitalWrite(in1, LOW);
+  //   digitalWrite(in2, HIGH);
+  //   rotDirection = 0;
+  //   delay(20);
+  // }
+
+  // I2C Comunication
+  if (count % 10 == 0)
   {
-    digitalWrite(in1, LOW);
-    digitalWrite(in2, HIGH);
-    rotDirection = 0;
-    delay(20);
+    Serial.println("Hello, Serial communication works");
   }
+  else
+  {
+    Serial.println(count);
+  }
+  count++;
 }
