d338e5aeaace66b6b84e826cb504d6d29d23f9ee
alonsocanov <alonsocanov@hotmail.com>
2021-04-09 16:13:01 -0500

    Removed stash 1

--- motor_control/motor_control.ino
+++ motor_control/motor_control.ino
@@ -8,43 +8,47 @@
 
 // right motor
 #define enB 10
-#define inB1 11
-#define inB2 12
+#define inB1 12
+#define inB2 11
 
 Motor motor_l = Motor(enA, inA1, inA2);
 Motor motor_r = Motor(enB, inB1, inB2);
 
 void setup()
 {
-
     //I2C COmunication
     // initialize serial:
     Serial.begin(9600);
+    Serial.println('Motor Control')
 }
 
 void loop()
 {
+    if (Serial.available())
+    {
+        char c = Serial.read();
+        if (c == '*')
+        {
+            Serial.print("Captured String is :");
+            Serial.println(data);
+            // Clear data
+            data = "";
+        }
+        else
+        {
+            data += c;
+        }
+    }
 
-    motor_l.forward(100);
-    motor_r.forward(100);
-    delay(900);
+    // motor_l.forward(100);
+    // motor_r.forward(100);
+    // delay(900);
 
-    motor_l.stop();
-    motor_r.stop();
-    delay(900);
+    // motor_l.stop();
+    // motor_r.stop();
+    // delay(900);
 
-    motor_l.backward(100);
-    motor_r.backward(100);
-    delay(900);
-
-    // I2C Comunication
-    // if (count % 10 == 0)
-    // {
-    //     Serial.println("Hello, Serial communication works");
-    // }
-    // else
-    // {
-    //     Serial.println(count);
-    // }
-    // count++;
+    // motor_l.backward(100);
+    // motor_r.backward(100);
+    // delay(900);
 }
