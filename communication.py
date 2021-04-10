f90b9b84e2c1cac7a5fb7c5759eb8b8a92908c76
alonsocanov <alonsocanov@hotmail.com>
2021-04-09 19:46:19 -0500

    Motor controll with pyserial

--- communication.py
+++ communication.py
@@ -2,18 +2,64 @@ import serial
 import time
 
 
-ser = serial.Serial(port='/dev/tty.usbmodem1433401',
-                    baudrate=115200, timeout=3)
+class Communication:
+    def __init__(self, port, baudrate=9600, timeout=.1):
+        self.__ser = serial.Serial(port=port,
+                                   baudrate=baudrate, timeout=timeout)
+        # self.__ser.write(str.encode('Connexion with arduino \n'))
+        self.__buff = None
+        time.sleep(2)
+
+    def send(self, data):
+        if not isinstance(data, str):
+            data = str(data)
+        rtxt = ''.join([data, '\n'])
+        self.__ser.write(str.encode(rtxt))
+        print("jetson: Message sent")
+
+    def receive(self):
+        if self.__ser.inWaiting() > 0:
+            recep = self.__ser.readline()[:-2]
+            try:
+                self.__buff = str(recep, 'utf-8')
+            except UnicodeDecodeError:
+                self.__buff = None
+
+        else:
+            self.__buff = None
+        return self.__buff
+
+    @property
+    def buffer(self):
+        return self.__buff
 
 
-counter = 32  # Below 32 everything in ASCII is gibberish
-while True:
-    counter += 1
-    # Convert the decimal number to ASCII then send it to the Arduino
-    ser.write(bytes(str(counter), 'utf-8'))
-    print(counter)
+def main():
+    port = '/dev/tty.usbmodem1433401'
+    baudrate = 9600
+    timeout = 3
+    arduino = Communication(port)
+    data = "10,20,30,40"
+    idx = 0
+    arduino.send(data)
+    while not arduino.buffer:
+        arduino.receive()
+        if arduino.buffer:
+            print(arduino.buffer)
+
+        # # Convert the decimal number to ASCII then send it to the Arduino
+        # ser.write(str.encode(data))
+        # # Delay for one tenth of a second
+        # time.sleep(.1)
+        idx += 1
+        # if idx == 100:
+        #     arduino.send_data(data)
+        #     time.sleep(.5)
+        #     arduino.receive_data()
+        #     # print(arduino.buffer)
+        #     time.sleep(.5)
+
+
     # Read the newest output from the Arduino
-    print(ser.readline().decode('utf-8'))
-    time.sleep(.1)  # Delay for one tenth of a second
-    if counter == 255:
-        counter = 32
+    # print(ser.readline().decode('utf-8'))
+main()
--- motor_control/motor_control.ino
+++ motor_control/motor_control.ino
@@ -11,6 +11,13 @@
 #define inB1 12
 #define inB2 11
 
+String data;
+String test;
+
+int ind1;
+float val_1;
+char av = 'a';
+
 Motor motor_l = Motor(enA, inA1, inA2);
 Motor motor_r = Motor(enB, inB1, inB2);
 
@@ -19,34 +26,38 @@ void setup()
     //I2C COmunication
     // initialize serial:
     Serial.begin(9600);
-    Serial.println('Motor Control')
+    motor_l.stop();
+    motor_r.stop();
 }
 
 void loop()
 {
     if (Serial.available())
     {
-        char c = Serial.read();
-        if (c == '*')
+
+        data = Serial.readStringUntil('\n');
+        Serial.print("Arduino: ");
+
+        Serial.println(data);
+        //finds location of first comma (,)
+        ind1 = data.indexOf(',');
+        test = data.substring(0, ind1);
+        val_1 = test.toFloat();
+
+        if (val_1 == 10.0)
         {
-            Serial.print("Captured String is :");
-            Serial.println(data);
-            // Clear data
-            data = "";
-        }
-        else
-        {
-            data += c;
+            motor_l.forward(100);
+            motor_r.forward(100);
+            delay(900)
         }
     }
+    else
+    {
+        motor_l.stop();
+        motor_r.stop();
+    }
 
-    // motor_l.forward(100);
-    // motor_r.forward(100);
-    // delay(900);
-
-    // motor_l.stop();
-    // motor_r.stop();
-    // delay(900);
+    //
 
     // motor_l.backward(100);
     // motor_r.backward(100);
